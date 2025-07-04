19e23849e1b213d297d6ab1383e967c395468ae3
Aya de Raghũl <nguyenlinhchi09@gmail.com>
2025-07-04 21:31:43 +0200

    update

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ let persons = [
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('post-data', (req) => {
   if (req.method === 'POST') return JSON.stringify(req.body);
